919d97d83c8936a6207e9727dc1de535dc7a64b9
shrinkunjpatel1010 <138668207+shrinkunjpatel1010@users.noreply.github.com>
2025-01-16 17:59:53 +0530

    Initial Commit

---
+++ LabVido/LabVidoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabVidoApp.swift
+//  LabVido
+//
+//  Created by SSLLP-126 on 16/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LabVidoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
